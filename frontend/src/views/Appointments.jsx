41832aaa32a8b8e99441851440d25dfbd3ddc842
Jas Aguilar <aguilarhans100701@gmail.com>
2023-03-28 08:27:17 +0800

    added appointment history

--- frontend/src/views/Appointments.jsx
+++ frontend/src/views/Appointments.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react';
 import useFetch from '../api/useFetch';
 import Header from '../components/Header';
+import LoadingSpinner from '../components/LoadingSpinner';
 import SideBar from '../components/SideBar';
 
 const Appointments = () => {
     const { loading, usersData } = useFetch("http://localhost:3001/patientsdata"); //^ get patients data
     const [appointmentHistory, setAppointmentHistory] = useState([]);
     const [patientsData, setPatientsData] = useState([]);
+    const [isHistoryClicked, setIsHistoryClicked] = useState(false);
+    const [appointmentFilter, setAppointmentFilter] = useState(null);
 
     useEffect(() => {
         if (usersData) {
@@ -15,17 +18,25 @@ const Appointments = () => {
     }, [usersData]);
 
     const handleApprove = (id) => {
-        const archivePatient = patientsData.map((patient) => {
-            if (patient.id === id) {
-                return patient;
-            } else {
-                return patient;
-            }
-        });
-        setPatientsData(archivePatient);
+        const patient = patientsData.find((patient) => patient.patientID === id);
+        if (patient) {
+            setPatientsData(patientsData.filter((patient) => patient.patientID !== id));
+            setAppointmentHistory((prev) => [...prev, patient]);
+            window.confirm("Do you want confirm " + patient.firstname + " " + patient.lastname + "?");
+        }
     };
 
-    if (loading) return <h1 className='text-2xl font-semibold'>Loading...</h1>
+    const handleHistory = () => {
+        setIsHistoryClicked(!isHistoryClicked);
+    }
+
+    if (loading) {
+        return (
+            <div className=' w-full h-screen flex justify-center items-center '>
+                <LoadingSpinner loading={loading} />
+            </div>
+        )
+    }
 
     return (
         <>
@@ -34,35 +45,59 @@ const Appointments = () => {
                 <div className='flex-2'>
                     <SideBar />
                 </div>
-
                 {/* APPOINTMENT WRAPPER */}
                 <div className='flex-1 p-4'>
                     <h1 className='font-semibold text-2xl mb-4'>APPOINTMENTS</h1>
-                    {/* APPOINTMENT TITLE CONTAINER */}
+                    <div className='mb-4 flex items-center gap-4'>
+                        <div className='flex py-1 px-2 bg-[#0AC1C9] text-white rounded gap-2 items-center'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="white" d="M6.865 6.882A7.25 7.25 0 1 1 4.75 12a.75.75 0 0 0-1.5 0a8.75 8.75 0 1 0 2.552-6.176a.756.756 0 0 0-.07.08L4.475 4.646a.5.5 0 0 0-.852.309L3.27 8.844a.5.5 0 0 0 .543.543l3.89-.354a.5.5 0 0 0 .307-.851L6.782 6.954a.757.757 0 0 0 .083-.072Z" /><path fill="white" d="M12.75 7a.75.75 0 0 0-1.5 0v5a.75.75 0 0 0 .352.636l3 1.875a.75.75 0 1 0 .796-1.272l-2.648-1.655V7Z" /></svg>
+                            <button
+                                onClick={() => handleHistory()}
+                            >History</button>
+                        </div>
 
+                        <div className='flex py-1 px-2 bg-[#0AC1C9] text-white rounded gap-2 items-center'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 16 16"><path fill="white" d="M.75 3h14.5a.75.75 0 0 1 0 1.5H.75a.75.75 0 0 1 0-1.5ZM3 7.75A.75.75 0 0 1 3.75 7h8.5a.75.75 0 0 1 0 1.5h-8.5A.75.75 0 0 1 3 7.75Zm3 4a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75Z" /></svg>
+                            <select
+                                value={appointmentFilter}
+                                className='bg-[#0AC1C9]'
+                                onChange={(e) => setAppointmentFilter(e.target.value)}
+                            >
+                                <option selected>Filter by</option>
+                                <option value="name">Name</option>
+                                <option value="case">Case</option>
+                                <option value="date">Date</option>
+                                <option value="time">Time</option>
+                                <option value="pending">Approved</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    {/* APPOINTMENT TITLE CONTAINER */}
                     <table className="w-full border-collapse border border-gray-400">
                         <thead className="bg-gray-200">
                             <tr>
-                                <th className="p-3 text-left">Name</th>
-                                <th className="p-3 text-left">Date &amp; Time</th>
-                                <th className="p-3 text-left">Case</th>
-                                <th className="p-3 text-left">Appointment Status</th>
-                                <th className="p-3 text-left">Payment Status</th>
-                                <th className="p-3 text-left">Payment Type</th>
-                                <th className="p-3 text-left">Actions</th>
+                                <th className="p-2 text-left">Name</th>
+                                <th className="p-2 text-left">Date &amp; Time</th>
+                                <th className="p-2 text-left">Case</th>
+                                <th className="p-2 text-left">Appointment Status</th>
+                                <th className="p-2 text-left">Payment Status</th>
+                                <th className="p-2 text-left">Payment Type</th>
+                                <th className="p-2 text-left">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
                             {
-                                patientsData.map(patient => (
+                                patientsData.map(patient =>
+                                (
                                     <tr className="bg-gray-100 " key={patient.patientID}>
-                                        <td className="p-3">{patient.firstname} {patient.lastname}</td>
-                                        <td className="p-3">{patient.appointmentDate} | {patient.appointmentTime}</td>
-                                        <td className="p-3">{patient.appointmentCase}</td>
-                                        <td className="p-3">{patient.appointmentStatus}</td>
-                                        <td className="p-3">{patient.paymentStatus}</td>
-                                        <td className="p-3">{patient.paymentType}</td>
-                                        <td className="p-3 flex items-center justify-around text-sm text-white">
+                                        <td className="p-2">{patient.firstname} {patient.lastname}</td>
+                                        <td className="p-2">{patient.appointmentDate} | {patient.appointmentTime}</td>
+                                        <td className="p-2">{patient.appointmentCase}</td>
+                                        <td className="p-2">{patient.appointmentStatus}</td>
+                                        <td className="p-2">{patient.paymentStatus}</td>
+                                        <td className="p-2">{patient.paymentType}</td>
+                                        <td className="p-2 flex items-center justify-around text-sm text-white">
                                             <div className='flex items-center p-1 px-2 bg-[#00cc66] rounded hover:bg-[#0bad5c] cursor-pointer transition-all active:bg-[#00cc66] ease-in'>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="white" d="m10.6 13.8l-2.175-2.175q-.275-.275-.688-.263t-.687.288q-.275.275-.275.7t.275.7L9.9 15.9q.275.275.7.275t.7-.275l5.675-5.675q.275-.275.263-.688t-.288-.687q-.275-.275-.7-.275t-.7.275L10.6 13.8ZM12 22q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Zm0-10Zm0 8q3.325 0 5.663-2.337T20 12q0-3.325-2.337-5.663T12 4Q8.675 4 6.337 6.337T4 12q0 3.325 2.337 5.663T12 20Z" /></svg>
                                                 <button
@@ -82,9 +117,22 @@ const Appointments = () => {
                                 ))
                             }
                         </tbody>
-                    </table>
-                </div>
-            </div>
+                    </table >
+                    {
+                        isHistoryClicked &&
+                        <div className='w-[35rem] m-auto flex flex-col gap-4 mt-7 bg-gray-400 p-6'>
+                            <h2 className='text-white'>History</h2>
+                            {
+                                appointmentHistory.map(patient => (
+                                    <div className='bg-gray-500 p-2 text-white rounded'>
+                                        <p>{patient.firstname} {patient.lastname} has finished the appointment.</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    }
+                </div >
+            </div >
         </>
     );
 }
